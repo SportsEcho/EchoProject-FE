ca77fb51c9eaf40dc4a2f5aaa91fb381b779981f
jinyr1128 <jinyr1128@gmail.com>
2024-01-11 00:13:37 +0900

    fix : 화면 출력 불가 해결

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import '../../assets/styles/index.css';
 
 function Footer() {
+  console.log("Rendering Footer");
   return (
       <footer>
         <p>&copy; 내배캠 Team Echo</p>
