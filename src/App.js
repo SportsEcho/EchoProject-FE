01b74a32c2d6aca486bf58acbc737997835148b9
jinyr1128 <jinyr1128@gmail.com>
2024-01-11 18:37:04 +0900

    feat : 각 종목 스케줄러 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
-import Calendar from './components/Calendar';
-import GamesSchedule from './components/GamesSchedule';
+import Calendar from './components/Calender/Calendar';
 import './assets/styles/index.css';
+import FootballSchedule from './components/GameSchedule/FootballSchedule';
+import BasketballSchedule from './components/GameSchedule/BasketballSchedule';
+import BaseballSchedule from './components/GameSchedule/BaseballSchedule';
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -14,7 +16,10 @@ function App() {
         <Header />
         <Navigation />
         <Calendar onDateChange={setSelectedDate} />
-        <GamesSchedule selectedDate={selectedDate.toISOString().split('T')[0]} />
+        <FootballSchedule footballGames={footballGames} />
+        <BasketballSchedule basketballGames={basketballGames} />
+        <BaseballSchedule baseballGames={baseballGames} />
+
         <Footer />
       </div>
   );
