e799284a22907da79e4b71b8cddcbc3ccc89e558
jinyr1128 <jinyr1128@gmail.com>
2024-01-11 16:58:52 +0900

    feat : App.js (캘린더 수정)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
-import Calendar from './components/Calender/Calendar';
+import Calendar from './components/Calendar';
+import GamesSchedule from './components/GamesSchedule';
 import './assets/styles/index.css';
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
       <div>
         <Header />
         <Navigation />
-        <Calendar />
-        {/* 여기에 경기 일정을 표시하는 컴포넌트를 추가 */}
+        <Calendar onDateChange={setSelectedDate} />
+        <GamesSchedule selectedDate={selectedDate.toISOString().split('T')[0]} />
         <Footer />
       </div>
   );
