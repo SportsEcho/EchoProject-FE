c180dd72a2c1338080046eef3176935e925680ab
jinyr1128 <jinyr1128@gmail.com>
2024-01-11 19:43:04 +0900

    git commit

--- src/api/gameApi.js
+++ src/api/gameApi.js
@@ -1,65 +1,45 @@
+import axios from 'axios';
 
 const fetchFootballGames = async (date) => {
-  const year = new Date(date).getFullYear(); // 연도 추출
+  const year = new Date(date).getFullYear();
   try {
-    const response = await fetch(`https://api-football-v1.p.rapidapi.com/v3/fixtures?date=${date}&league=39&season=${year}`, {
-      method: 'GET',
+    const response = await axios.get(`https://api-football-v1.p.rapidapi.com/v3/fixtures?date=${date}&league=39&season=${year}`, {
       headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Key': 'd789e7aa74msh95a2867cc80a6d0p11239ajsna2c01db4ee85',
         'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-      },
+      }
     });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    return data.response; // 경기 정보 반환
+    return response.data.response;
   } catch (error) {
     console.error('Error fetching football data:', error);
   }
 };
 
 const fetchBasketballGames = async (date) => {
-  const year = new Date(date).getFullYear(); // 연도 추출
+  const year = new Date(date).getFullYear();
   try {
-    const response = await fetch(`https://api-basketball.p.rapidapi.com/games?date=${date}&league=12&season=${year}`, {
-      method: 'GET',
+    const response = await axios.get(`https://api-basketball.p.rapidapi.com/games?date=${date}&league=12&season=${year}`, {
       headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Key': 'd789e7aa74msh95a2867cc80a6d0p11239ajsna2c01db4ee85',
         'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com',
-      },
+      }
     });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    return data.response; // 경기 정보 반환
+    return response.data.response;
   } catch (error) {
     console.error('Error fetching basketball data:', error);
   }
 };
 
 const fetchBaseballGames = async (date) => {
-  const year = new Date(date).getFullYear(); // 연도 추출
+  const year = new Date(date).getFullYear();
   try {
-    const response = await fetch(`https://api-baseball.p.rapidapi.com/games?date=${date}&league=1&season=${year}`, {
-      method: 'GET',
+    const response = await axios.get(`https://api-baseball.p.rapidapi.com/games?date=${date}&league=1&season=${year}`, {
       headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Key': 'd789e7aa74msh95a2867cc80a6d0p11239ajsna2c01db4ee85',
         'X-RapidAPI-Host': 'api-baseball.p.rapidapi.com',
-      },
+      }
     });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    return data.response; // 경기 정보 반환
+    return response.data.response;
   } catch (error) {
     console.error('Error fetching baseball data:', error);
   }
