70e7631d10e9295d70167aa2eef30dd5dc54c8d7
jinyr1128 <jinyr1128@gmail.com>
2024-01-11 15:41:14 +0900

    feat : 달력기능 import 추가

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
+import Calendar from './components/Calendar';
 import './assets/styles/index.css';
-
-
 
 function App() {
   return (
       <div>
         <Header />
         <Navigation />
+        <Calendar />
+        {/* 여기에 경기 일정을 표시하는 컴포넌트를 추가 */}
         <Footer />
       </div>
   );
