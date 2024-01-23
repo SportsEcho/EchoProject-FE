b3bb5e0099627ffb8dcb9124011b4f2484581394
zzzzseong <jung48182@gmail.com>
2024-01-23 19:13:58 +0900

    fix(OAuth): 카카오, 네이버 OAuth 안되는 오류 해결

--- src/components/OAuth/KakaoRedirect.js
+++ src/components/OAuth/KakaoRedirect.js
@@ -1,36 +1,41 @@
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const KakaoRedirect = async () => {  
+const KakaoRedirect = () => {
+  const navigate = useNavigate();
 
-    const code = new URL(window.location.href).searchParams.get("code");
+  useEffect(() => {
+    const fetchData = async () => {
+      const code = new URL(window.location.href).searchParams.get("code");
 
-    const navigate = useNavigate();
-  
-    try {
-        const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/members/kakao/callback?code=${code}`);
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/members/kakao/callback?code=${code}`);
 
         if (response.status === 200) {
-            const authToken = response.headers.authorization || response.headers.Authorization;
-            const refreshToken = response.headers.refreshauthorization || response.headers.Refreshauthorization;
-  
-            // 로컬 스토리지에 토큰 저장
-            if (authToken) {
-            localStorage.setItem('authToken', authToken.split(' ')[1]); // 'Bearer '을 제거하고 토큰만 저장
-            }
-            if (refreshToken) {
-            localStorage.setItem('refreshToken', refreshToken.split(' ')[1]); // 'Bearer '을 제거하고 토큰만 저장
-            }
+          const authToken = response.headers.authorization || response.headers.Authorization;
+          const refreshToken = response.headers.refreshauthorization || response.headers.Refreshauthorization;
 
-            // 메인 페이지로 리디렉션
-            navigate('/');
+          // 로컬 스토리지에 토큰 저장
+          if (authToken) {
+            localStorage.setItem('authToken', authToken.split(' ')[1]); // 'Bearer '을 제거하고 토큰만 저장
+          }
+          if (refreshToken) {
+            localStorage.setItem('refreshToken', refreshToken.split(' ')[1]); // 'Bearer '을 제거하고 토큰만 저장
+          }
+
+          // 메인 페이지로 리디렉션
+          navigate('/');
         }
-    } catch(error) {
+      } catch (error) {
         console.log(error);
-    }
-      
-    return <div>로그인 중입니다.</div>;
-  };
-  
-  export default KakaoRedirect;
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+
+  return <div>로그인 중입니다.</div>;
+};
+
+export default KakaoRedirect;
