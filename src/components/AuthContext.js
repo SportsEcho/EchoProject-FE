757d55bfd5301ff6d984782f965b1a0e8aec0522
jinyr1128 <jinyr1128@gmail.com>
2024-01-24 02:58:18 +0900

    feat : useEffect

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
 
 // AuthContext 생성
 const AuthContext = createContext(null);
