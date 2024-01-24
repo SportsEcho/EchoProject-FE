22b125b0e8a88d9a3966021f1570ee15f2c1bb14
jinyr1128 <jinyr1128@gmail.com>
2024-01-24 23:42:08 +0900

    feat : 무한스크롤 반응이 느려서 200ms마다 설정

--- src/components/Product/ProductPage.js
+++ src/components/Product/ProductPage.js
@@ -12,6 +12,7 @@ function ProductPage() {
   const itemsPerPage = 20;
   const [hasMore, setHasMore] = useState(true);
 
+  // `useCallback`에 의존성 추가: currentPage와 itemsPerPage
   const fetchProducts = useCallback(async (page) => {
     setIsLoading(true);
     try {
@@ -28,7 +29,12 @@ function ProductPage() {
         },
         headers
       });
-      return response.data.data || [];
+      // 데이터가 비어있으면 hasMore을 false로 설정
+      const newProducts = response.data.data || [];
+      if (newProducts.length < itemsPerPage) {
+        setHasMore(false);
+      }
+      return newProducts;
     } catch (error) {
       setError('상품을 불러오는 데 실패했습니다.');
       console.error("Error fetching products", error);
@@ -36,29 +42,28 @@ function ProductPage() {
     } finally {
       setIsLoading(false);
     }
-  }, []);
+  }, [currentPage, itemsPerPage]);
 
   useEffect(() => {
     fetchProducts(currentPage).then(newProducts => {
       setProducts(prevProducts => [...prevProducts, ...newProducts]);
-      setHasMore(newProducts.length === itemsPerPage);
     });
   }, [currentPage, fetchProducts]);
 
-// 스크롤 이벤트 핸들러
+  // 스크롤 이벤트 핸들러
   const handleScroll = useCallback(throttle(() => {
     if (window.innerHeight + document.documentElement.scrollTop < document.documentElement.offsetHeight || isLoading || !hasMore) {
       return;
     }
     setCurrentPage(prevPage => prevPage + 1);
-  }, 200), [isLoading, hasMore]); //200ms 간격으로 이벤트를 처리
+  }, 200), [isLoading, hasMore]); // 200ms 간격으로 이벤트를 처리
 
   useEffect(() => {
     // 스크롤 이벤트 리스너 추가
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       // 컴포넌트 언마운트 시 스크롤 이벤트 리스너 제거
+      handleScroll.cancel(); // throttle을 취소하는 메소드 호출
       window.removeEventListener('scroll', handleScroll);
     };
   }, [handleScroll]);
