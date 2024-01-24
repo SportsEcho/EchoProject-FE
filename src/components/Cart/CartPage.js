9fa21233197d1d678eeca6444c297a5f2703ff62
jinyr1128 <jinyr1128@gmail.com>
2024-01-24 20:30:11 +0900

    fix : 장바구니 삭제와 이미지 요청 수정

--- src/components/Cart/CartPage.js
+++ src/components/Cart/CartPage.js
@@ -18,7 +18,7 @@ function CartPage() {
         const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/carts`, {
           headers: { Authorization: `Bearer ${authToken}` }
         });
-        setCartItems(response.data); // 데이터 구조에 맞게 수정
+        setCartItems(response.data.data); // 응답 구조에 맞게 수정
       } catch (error) {
         console.error("장바구니 정보 조회 중 오류 발생: ", error);
       }
@@ -33,7 +33,7 @@ function CartPage() {
       await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/api/carts/products/${productId}`, {
         headers: { Authorization: `Bearer ${authToken}` }
       });
-      setCartItems(cartItems.filter(item => item.productId !== productId));
+      setCartItems(cartItems.filter(item => item.id !== productId)); // 'id'로 변경
       alert('상품이 장바구니에서 삭제되었습니다.');
     } catch (error) {
       console.error("장바구니 삭제 중 오류 발생: ", error);
@@ -41,10 +41,7 @@ function CartPage() {
     }
   };
 
-  const handleGoToOrder = () => {
-    navigate('/order'); // 주문 페이지로 이동
-  };
-  const totalPrice = cartItems.reduce((total, item) => total + (item.productsQuantity * item.price), 0);
+  const totalPrice = cartItems.reduce((total, item) => total + (item.quantity * item.price), 0);
 
   if (!cartItems.length) return <div>장바구니가 비어있습니다.</div>;
 
@@ -52,19 +49,14 @@ function CartPage() {
       <div>
         <h1>장바구니</h1>
         {cartItems.map(item => (
-            <div key={item.productId}>
-              {/* imageUrlList 배열이 존재하고 비어 있지 않은 경우에만 이미지를 표시 */}
+            <div key={item.id}>
               {item.imageUrlList && item.imageUrlList.length > 0 && (
-                  <img
-                      src={item.imageUrlList[0]}
-                      alt={item.productName}
-                      style={{ width: '50px', height: '50px' }}
-                  />
+                  <img src={item.imageUrlList[0]} alt={item.title} style={{ width: '50px', height: '50px' }} />
               )}
-              <p>{item.productName}</p>
-              <p>수량: {item.productsQuantity}</p>
+              <p>{item.title}</p>
+              <p>수량: {item.quantity}</p>
               <p>가격: {item.price}원</p>
-              <button onClick={() => handleDelete(item.productId)}>삭제하기</button>
+              <button onClick={() => handleDelete(item.id)}>삭제하기</button> {/* 'item.id' 사용 */}
             </div>
         ))}
         <p>총합: {totalPrice}원</p>
